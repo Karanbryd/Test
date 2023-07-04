f23e6d2875fade7441b216b7a627889fc2ada04e
Karanbryd <138604080+Karanbryd@users.noreply.github.com>
2023-07-04 21:31:35 +0530

    Update Program-3.java

--- Program-3.java
+++ Program-3.java
@@ -1,1 +1,26 @@
+package Project;
+
+import java.util.Scanner;
+
+public class NumberSeries2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the value of x: ");
+        int x = scanner.nextInt();
+        
+        System.out.print("Output: ");
+        
+        for (int i = 1; i <= x; i += 2) 
+        	{
+              System.out.print(i);
+                if (i + 2 <= x) 
+                {
+                   System.out.print(", ");
+                 }
+            }
+    }
+}
 
